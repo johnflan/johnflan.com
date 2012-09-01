4b3271afe4200ce374ebe1b2166338905a5e92a2
John Flanagan <johnflan@gmail.com>
2012-09-01 15:49:00 +0100

    Blog post: facebook unexpected user behaviour

--- deploy.sh
+++ deploy.sh
@@ -1,4 +1,4 @@
 #! /bin/bash
 hyde gen
-rsync -zvr --delete deploy/ johnflan.com:/var/www/johnflan.com/
+rsync -zvr --delete --stats --progress --exclude 'scratch' deploy/ johnflan.com:/var/www/johnflan.com/
 rm -fr deploy/
