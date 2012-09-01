5c5e507abf5b4652352430461f0ae9ef137b5629
John Flanagan <johnflan@gmail.com>
2012-09-01 21:30:14 +0100

    Updated deploy script to fix analytics

--- deploy.sh
+++ deploy.sh
@@ -1,4 +1,10 @@
 #! /bin/bash
+mv site.yaml site_dev.yaml 
+mv site_prod.yaml site.yaml
+
 hyde gen
 rsync -zvr --delete --stats --progress --exclude 'scratch' deploy/ johnflan.com:/var/www/johnflan.com/
 rm -fr deploy/
+
+mv site.yaml site_prod.yaml
+mv site_dev.yaml site.yaml
